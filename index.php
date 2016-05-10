ffd7d29fffa6f46aeab6f605bc892ceb8debd194
jhwebtech <julieh71@ymail.com>
2016-05-10 14:09:10 -0500

    Initial repo

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>JHWebTech</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/main2.css">
+		<link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+<body>
+
+	<header>
+
+		
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		  	<div class="row">
+		  		
+		  		<!-- Top Navigation -->
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			      <a class="navbar-brand" href="index.php">JHWebtech</a>
+			    </div>
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">     
+			      <ul class="nav navbar-nav navbar-right">
+			        <li><a href="#about">About</a></li>
+			        <li><a href="#contact">Contact</a></li>
+			        <li><a href="https://github.com/jhwebtech" target="_blank">Github</a></li>
+			        <li><a href="https://www.linkedin.com/in/juliehodgson2" target="_blank">Linkedin</a></li>
+			        <li class="dropdown">
+			          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog<span class="caret"></span></a>
+			          <ul class="dropdown-menu">
+			            <li><a href="#">Java</a></li>
+			            <li><a href="#">PHP</a></li>
+			            <li><a href="#">Wordpress</a></li>
+			            <li role="separator" class="divider"></li>
+			            <li><a href="#">Javascript</a></li>
+			          </ul>
+			        </li>
+			      </ul>
+		       </div><!-- /.navbar-collapse -->
+		    
+		</nav>
+
+		<div class="jumbotron">
+			<h1>Web Developer</h1><br/>
+			<h3>HTML &#8226; CSS &#8226; Javascript &#8226; Java &#8226; PHP &#8226; Ruby on Rails</h3>
+	    </div>
+
+	
+	    </div><!-- end row -->
+		</div><!-- /.container-fluid -->
+
+	</header>
+
+		<div class="container-fluid" id="about">
+			<div class="row">
+				<div class="col-xs-8 col-sm-8 col-md-12">
+					<article>
+					<h3 class="titles">About Me</h3>
+					<h3>Hi, My Name is Julie Hodgson</h3>
+					<p>I am a Web Developer who is currently pursuing 
+						a master’s degree in Computer Science from the
+						 University of Illinois Springfield.</p>
+					<br/>
+					<h3 class="titles">Technologies</h3>
+						<ul>
+							<li>Node.js</li>
+							<li>Ruby on Rails</li>
+							<li>Wordpress</li>
+							<li>Java</li>
+							<li>Python</li>
+							<li>HTML</li>
+							<li>CSS</li>
+						</ul>
+					</article>  
+				</div><!-- columns -->  
+			</div><!-- row -->
+		</div><!--container -->
+<article id="contact">
+	
+</article>
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</div>
+</body>
+</html>
